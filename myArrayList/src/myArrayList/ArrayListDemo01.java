fb11da2693d6d804b035f2fdcd158fa2e9d0bf94
ZhouWeiPeng1 <2250837022@qq.com>
2021-11-04 19:55:33 +0800

    java study

---
+++ myArrayList/src/myArrayList/ArrayListDemo01.java
@@ -0,0 +1,33 @@
+package myArrayList;
+/*
+ 		ArrayList构造方法：
+ 			public ArrayList();创建一个空的集合对象
+ 			
+ 		ArrayList添加方法：
+ 		public boolean add(E e);将指定的元素追加到此集合的末尾
+ 		public void add(int index,E element);在此集合中的指定位置插入指定的元素
+ */
+import java.util.ArrayList;
+public class ArrayListDemo01 {
+	public static void main(String[] args) {
+		//public ArrayList();创建一个空的集合对象
+		//ArrayList<String> array=new ArrayList<>();
+		ArrayList<String> array=new ArrayList<String>();
+		
+		//public boolean add(E e);将指定的元素追加到此集合的末尾
+//		System.out.println(array.add("hello"));
+		
+		array.add("hello");
+		array.add("world");
+		array.add("java");
+		
+		//public void add(int index,E element);在此集合中的指定位置插入指定的元素
+		array.add(1,"javase");
+		array.add(3,"javaee");
+		//array.add(4,"javase";) //报错IndexOutOfBoundsException 集合的索引越界
+		
+		//输出集合
+		System.out.println("array:"+array);
+		
+	}
+}
