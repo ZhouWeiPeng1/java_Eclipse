c6389f31bf250a1814ee7d59b81fe34f418fa675
ZhouWeiPeng1 <2250837022@qq.com>
2021-11-08 20:10:37 +0800

    java study

---
+++ 继承概述/src/继承概述/Fu.java
@@ -0,0 +1,7 @@
+package 继承概述;
+
+public class Fu {
+	public void show() {
+		System.out.println("show方法被调用");
+	}
+}
---
+++ 继承概述/src/继承概述/Zi.java
@@ -0,0 +1,7 @@
+package 继承概述;
+
+public class Zi extends Fu {
+	public void method() {
+		System.out.println("mothod方法被调用");
+	}
+}
