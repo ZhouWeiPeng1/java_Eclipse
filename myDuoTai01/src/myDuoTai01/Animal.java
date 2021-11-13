7332fbc60d0d9abe0a2a3fd0ea8207b3e0fb0e4a
ZhouWeiPeng1 <2250837022@qq.com>
2021-11-13 19:07:33 +0800

    java study

---
+++ myDuoTai01/src/myDuoTai01/Animal.java
@@ -0,0 +1,7 @@
+package myDuoTai01;
+
+public class Animal {
+	public void eat() {
+		System.out.println("动物吃东西");
+	}
+}
---
+++ myDuoTai01/src/myDuoTai01/Cat.java
@@ -0,0 +1,7 @@
+package myDuoTai01;
+
+public class Cat extends Animal{
+	public void eat() {
+		System.out.println("√®≥‘”„");
+	}
+}
