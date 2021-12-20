1adb30d2204af3785016b7cf4b9db0dfb0f06f43
ZhouWeiPeng1 <2250837022@qq.com>
2021-12-20 19:08:06 +0800

    java study

---
+++ 异常try_catch/src/异常try_catch/Demo.java
@@ -0,0 +1,27 @@
+package 异常try_catch;
+
+/*
+ 	try{
+ 		可能出现异常的代码;
+ 	} catch(异常类名 变量名){
+ 		异常的处理代码;
+ 	}
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("开始");
+		method();
+		System.out.println("结束");
+	}
+	
+	public static void method() {
+		try {
+			int[] arr = {1,2,3};
+			System.out.println(arr[3]);
+		}catch(ArrayIndexOutOfBoundsException e) {
+//			System.out.println("你访问的数组的索引不存在");
+			e.printStackTrace();
+		}
+		
+	}
+}
